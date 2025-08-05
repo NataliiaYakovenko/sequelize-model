1959de74c74bf72dc645a1b5d51c88dbb7605105
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-08-05 21:29:10 +0300

    Add model and migrations

---
+++ migrations/20250805175840-create-phone.js
@@ -0,0 +1,58 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("phones", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      brand: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      model: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      productionYear: {
+        field: "production_year",
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      ramSize: {
+        field: "ram_size",
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      processor: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      screenSize: {
+        field: "screen_size",
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+      },
+      isNfc: {
+        field: "is_nfc",
+        type: Sequelize.BOOLEAN,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: "created_at",
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        field: "updated_at",
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Phones");
+  },
+};
---
+++ models/Phone.js
@@ -0,0 +1,78 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Phone extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Phone.init(
+    {
+      brand: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: true,
+          notEmpty: true,
+        },
+      },
+      model: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: true,
+          notEmpty: true,
+        },
+      },
+      productionYear: {
+        field: "production_year",
+        type: DataTypes.DATEONLY,
+        allowNull: false,
+        validate: {
+          isDate: true,
+          isBefore: new Date().toDateString(),
+        },
+      },
+      ramSize: {
+        field: "ram_size",
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          min: 0,
+        },
+      },
+      processor: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          notNull: true,
+          notEmpty: true,
+        },
+      },
+      screenSize: {
+        field: "screen_size",
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+        validate: {
+          min: 0,
+        },
+      },
+      isNfc: {
+        field: "is_nfc",
+        type: DataTypes.BOOLEAN,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Phone",
+      tableName: "phones",
+      underscored: true,
+    }
+  );
+  return Phone;
+};
