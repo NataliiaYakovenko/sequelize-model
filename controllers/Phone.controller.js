b4768832d5cad0880b6c54031130bd685b4b84ab
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-08-06 11:46:15 +0300

    Get all phones from 3 page

--- controllers/Phone.controller.js
+++ controllers/Phone.controller.js
@@ -9,3 +9,22 @@ module.exports.createPhone = async (req, res, next) => {
     next(error);
   }
 };
+
+module.exports.getAllPhones = async (req, res, next) => {
+  try {
+    const { page = 1, results = 10 } = req.query;
+    const limit = Number(results);
+    const offset = (page - 1) * limit;
+
+    const resultArray = await Phone.findAll({
+      limit,
+      offset,
+      order: [["productionYear","ASC"]],
+    });
+    return res.status(200).send(resultArray);
+  } catch (error) {
+    next(error);
+  }
+};
+
+
