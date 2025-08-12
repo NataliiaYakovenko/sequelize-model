b7765849cc265ce2724d65c1cba35a27eab5bc11
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-08-12 13:24:27 +0300

    Add checks to modelPhController

--- controllers/ModelPh.controller.js
+++ controllers/ModelPh.controller.js
@@ -4,6 +4,9 @@ module.exports.createModelPh = async (req, res, next) => {
   try {
     const { body } = req;
     const createdModelPh = await ModelPh.create(body);
+    if (!createdModelPh) {
+      return res.status(400).send("Something wrong");
+    }
     return res.status(201).send(createdModelPh);
   } catch (error) {
     next(error);
